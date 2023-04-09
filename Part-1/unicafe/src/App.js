8a6e6414ac53c233fd6571d3e0404a56df766e93
Nairbuul <luu_brian@ymail.com>
2023-04-09 16:33:55 -0700

    Exercise 1.7 Unicafe pt 2

--- Part-1/unicafe/src/App.js
+++ Part-1/unicafe/src/App.js
@@ -1,8 +1,7 @@
 import { useState } from 'react'
 
-//Exercise 1.6) Unicafe Part 1. 
-//          Create a website that has three buttons that'll report the feedback of the unicafe.
-//          Then display the statistics onto the web page.
+//Exercise 1.7) Unicafe Part 2. 
+//            Display the average and the percentage of positive.
 
 //Creating a component that'll create a header.
 const Header = ({title}) => (<h1>{title}</h1>)
@@ -17,15 +16,12 @@ const Button = ({handleClick, text}) => (
   </button>
 )
 
-
-//This doesn't work figure out why later.
-// const ArrayOfButtons = (props) => (
-//   <div>
-//     <Button onClick={props.array[0]} text={props.array[1]}/>
-//     <Button onClick={props.array[2]} text={props.array[3]}/>
-//     <Button onClick={props.array[4]} text={props.array[5]}/>
-//   </div>
-// )
+//Component that'll display the percentage.
+const Percentage = (props) => (
+  <div>
+    {props.text} {props.percentage}%
+  </div>
+)
 
 const App = () => {
   // save clicks of each button to its own state
@@ -33,16 +29,19 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  //Functions incrementing good/neutral/bad
   const incrementGood = () => {setGood(good +  1)}
   const incrementNeutral =  () => {setNeutral(neutral + 1)}
   const incrementBad = () => {setBad(bad + 1)}
 
-  // const array=[incrementGood, 'good', incrementBad, 'Bad', incrementNeutral, 'Neutral']
+  //Variables for (amount of clicks), (average), (perctange of positive responses).
+  const total = good+neutral+bad
+  const average = total/3
+  const positivePercentage = (good/total)*100
 
   return (
     <div>
       <Header title={"give feedback"}/>
-      {/* <ArrayOfButtons array={array}/> */}
       <Button handleClick={incrementGood} text='good'/>
       <Button handleClick={incrementNeutral} text='neutral'/>
       <Button handleClick={incrementBad} text='bad'/>
@@ -50,6 +49,9 @@ const App = () => {
       <Display value={good} name='good'/>
       <Display value={neutral} name='neutral'/>
       <Display value={bad} name='bad'/>
+      <Display value={total} name='total:'/>
+      <Display value={average} name='average'/>
+      <Percentage percentage={positivePercentage} text="Positive:"/>
     </div>
   )
 }
